37bca98b34b9ef1fbdd85ae35b942c8e57442eaa
Tae-Jin Oh <taejinoh@gmail.com>
2020-11-06 11:05:23 +0900

    changed to force readLine

--- Friday-algorithm/p1000/p1000/main.swift
+++ Friday-algorithm/p1000/p1000/main.swift
@@ -9,23 +9,24 @@ import Foundation
 
 // https://www.acmicpc.net/problem/1000
 
-func p1000() -> Int {
+func p1000() {
     print("Input two positive integers less than 10, separated with a space")
-    let inStr = readLine()
-    guard (inStr ?? "").contains(" ") && (inStr ?? "").split(separator:" ").count == 2 else {
+    let inStr = readLine()!
+    guard inStr.contains(" ") && inStr.split(separator:" ").count == 2 else {
         print("Error, must input two positive integers less than 10, separated by a space")
         return p1000()
     }
+    
     var out = 0
     
-    for ii in (inStr ?? "").split(separator: " ") {
+    for ii in inStr.split(separator: " ") {
         guard Int(ii) != nil && Int(ii)! > 0 && Int(ii)! < 10 else {
             print("Error, must input two positive integers less than 10, separated by a space")
             return p1000()
         }
         out += Int(ii)!
     }
-    return out
+    print(out)
 }
 
-print(p1000())
+p1000()
